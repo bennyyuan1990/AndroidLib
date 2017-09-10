dff388653520c9e189b4989c45f0d26940ae78aa
Benny <bennyyuan1990@sina.com>
2017-09-10 10:42:07 +0800

    ColorTrackTextView

--- App/src/main/java/com/benny/libapp/TestActivity.java
+++ App/src/main/java/com/benny/libapp/TestActivity.java
@@ -1,11 +1,13 @@
 package com.benny.libapp;
 
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.benny.baselib.view.progress.ArcProgress;
+import com.benny.baselib.view.text.ColorTrackTextView;
 
 /**
  * Created by Benny on 2017/9/9.
@@ -23,5 +25,14 @@ public class TestActivity extends AppCompatActivity {
         objectAnimator.setStartDelay(1000);
         objectAnimator.setDuration(10000);
         objectAnimator.start();
+
+
+        ColorTrackTextView textView = (ColorTrackTextView) findViewById(R.id.activity_test_color_text_view);
+        ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(textView,"Step",0,1.0f);
+        objectAnimator1.setStartDelay(1000);
+        objectAnimator1.setDuration(1000);
+        objectAnimator1.setRepeatCount(ValueAnimator.INFINITE);
+        objectAnimator1.setRepeatMode(ValueAnimator.REVERSE);
+        objectAnimator1.start();
     }
 }
---
+++ baselib/src/main/java/com/benny/baselib/view/text/ColorTrackTextView.java
@@ -0,0 +1,72 @@
+package com.benny.baselib.view.text;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.benny.baselib.R;
+
+/**
+ * Created by Benny on 2017/9/10.
+ */
+
+public class ColorTrackTextView extends android.support.v7.widget.AppCompatTextView {
+    private Paint mPaint;
+    private int mCoverColor = Color.GREEN;
+    private float mStep = 0.5f;
+
+    public void setStep(float step) {
+        if (step < 0) step = 0;
+        else if (step > 1) step = 1;
+        this.mStep = step;
+        Log.d("ColorTrackTextView", "setStep: " + this.mStep);
+        postInvalidate();
+    }
+
+    public ColorTrackTextView(Context context) {
+        this(context, null);
+    }
+
+    public ColorTrackTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ColorTrackTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mPaint = new Paint(getPaint());
+
+        TypedArray typedArray = context.obtainStyledAttributes(R.styleable.ColorTrackTextView);
+        mCoverColor = typedArray.getColor(R.styleable.ColorTrackTextView_coverColor, mCoverColor);
+        typedArray.recycle();
+
+        mPaint.setColor(mCoverColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        float textWidth = getPaint().measureText(getText().toString());
+        LinearGradient linearGradient = new LinearGradient(0, getHeight() / 2,
+                textWidth, getHeight() / 2,
+                new int[]{getTextColors().getDefaultColor(), mCoverColor, getTextColors().getDefaultColor()},
+                new float[]{0, 0.5f, 1}, Shader.TileMode.CLAMP);
+        Matrix matrix = new Matrix();
+        matrix.setTranslate((mStep*2 - 1) * textWidth, 0);
+        linearGradient.setLocalMatrix(matrix);
+        getPaint().setShader(linearGradient);
+
+        super.onDraw(canvas);
+    }
+}
