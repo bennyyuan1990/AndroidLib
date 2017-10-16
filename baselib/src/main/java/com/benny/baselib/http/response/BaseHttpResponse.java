5e7b4da7af7500086355e77dc79d44a86a198a17
Benny <bennyyuan1990@sina.com>
2017-10-16 22:26:16 +0800

    Http

--- baselib/src/main/java/com/benny/baselib/http/response/BaseHttpResponse.java
+++ baselib/src/main/java/com/benny/baselib/http/response/BaseHttpResponse.java
@@ -1,9 +1,18 @@
 package com.benny.baselib.http.response;
 
+import android.support.annotation.NonNull;
+
+import com.benny.baselib.http.interfaces.IHttpListener;
+
 /**
  * Created by yuanbb on 2017/9/12.
  */
 
-public class BaseHttpResponse {
+public class BaseHttpResponse implements IHttpResponse {
 
+    @NonNull
+    @Override
+    public IHttpListener getHttpListener() {
+        return null;
+    }
 }
